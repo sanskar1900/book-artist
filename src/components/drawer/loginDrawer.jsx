1de38cae717e0f9234fbe588b692ba9b7bbc33b2
Sanskar Bhadauriya <sanskar1900@Sanskars-MacBook-Pro.local>
2025-01-04 22:50:20 +0530

    progress

--- src/components/drawer/loginDrawer.jsx
+++ src/components/drawer/loginDrawer.jsx
@@ -3,9 +3,11 @@ import RightDrawer from ".";
 import { closeLoginDrawer, openLoginDrawer } from "../../redux/uiSlice";
 import classes from "./drawer.module.css";
 import { useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Button, TextField } from "@mui/material";
 
 const LoginDrawer = () => {
+  const [formData, setFormData] = useState({ email: "", password: "" });
   const location = useLocation();
   const showLoginDrawer = useSelector((state) => state.login.showLoginDrawer);
   const dispatch = useDispatch();
@@ -19,10 +21,72 @@ const LoginDrawer = () => {
     }
   }, [location]);
 
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const checkLoginButtonDisability = () => {
+    return formData.email.length === 0 || formData.password.length === 0;
+  };
+
   const LoginDrawerUI = () => {
     return (
       <div className={classes.loginDrawerRoot}>
-        <h2>Login form will come here</h2>
+        <TextField
+          id="outlined-basic"
+          label="Enter Email"
+          variant="standard"
+          value={formData.email}
+          type="email"
+          name="email"
+          onChange={handleInputChange}
+          sx={{
+            input: { color: "white" },
+            label: { color: "white" },
+            "& .MuiInput-underline:before": { borderBottomColor: "white" },
+            "& .MuiInput-underline:hover:before": {
+              borderBottomColor: "white",
+            },
+            "& .MuiInput-underline:after": { borderBottomColor: "white" },
+          }}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Enter Password"
+          variant="standard"
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleInputChange}
+          sx={{
+            input: { color: "white" },
+            label: { color: "white" },
+            "& .MuiInput-underline:before": { borderBottomColor: "white" },
+            "& .MuiInput-underline:hover:before": {
+              borderBottomColor: "white",
+            },
+            "& .MuiInput-underline:after": { borderBottomColor: "white" },
+          }}
+        />
+        <Button
+          sx={{
+            backgroundColor: "white",
+            color: "black",
+            marginTop: "16px",
+            "&.Mui-disabled": {
+              backgroundColor: "gray",
+              color: "white",
+            },
+          }}
+          variant="contained"
+          disabled={checkLoginButtonDisability()}
+        >
+          Login
+        </Button>
       </div>
     );
   };
